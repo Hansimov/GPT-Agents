d54194835b4c90edc37cd804a398f20859e521ad
Hansimov <591172499@qq.com>
2023-06-09 19:36:16 +0800

    [Backup] To multi-line output async for multiple agents

--- tests/test_async_run.py
+++ tests/test_async_run.py
@@ -1,6 +1,8 @@
 import asyncio
 import concurrent.futures
 import random
+import shutil
+import textwrap
 import time
 
 
@@ -9,22 +11,31 @@ class LLMAgent:
         self.model = model
 
     async def answer(self, question=None):
-        msg = f"[{self.model}]: I am {self.model}. "
+        word_num = random.randint(20, 30)
+        msg = f"[{self.model}]: I am {self.model}. ({word_num} words) "
         print(msg)
-        for i in range(5):
-            rand_num = random.random() * 0.5
-            msg += f"<{self.model}_holder_{round(rand_num,2)}>"
+        for i in range(word_num):
+            rand_num = random.random() * 0.3
+            msg += f"<{self.model}_word_{i}>"
             await asyncio.sleep(rand_num)
-            print(
-                f"\033[{self.index}A\033[2K{msg}\033[{self.index}B",
-                end="\r",
+            lines = textwrap.wrap(
+                msg,
+                width=shutil.get_terminal_size().columns,
             )
+            self.task_runner.max_lines = max(self.task_runner.max_lines, len(lines))
+            for j, line in enumerate(lines):
+                index = self.index + j
+                line = lines[j] if j < len(lines) else ""
+                print(
+                    f"\033[{index + j}A\033[2K{line}\033[{index + j}B",
+                    end="\r",
+                )
         return msg
 
 
 class TaskRunner:
     def __init__(self):
-        pass
+        self.max_lines = 0
 
     async def ask_question(self, question="Hello, who are you?"):
         print(question)
@@ -38,7 +49,8 @@ class TaskRunner:
         ]
         agents = [LLMAgent(model) for model in models]
         for i, agent in enumerate(agents):
-            agent.index = len(models) - i
+            agent.index = i * self.max_lines + 1
+            agent.task_runner = self
         tasks = [asyncio.create_task(agent.answer(question)) for agent in agents]
         await asyncio.gather(*tasks)
 
