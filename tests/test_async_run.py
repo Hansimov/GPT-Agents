30a2616a90595df951b9916d948cbc3c68e506b7
Hansimov <591172499@qq.com>
2023-06-14 17:04:19 +0800

    [Feature] Print single agent

--- tests/test_async_run.py
+++ tests/test_async_run.py
@@ -4,63 +4,100 @@ import concurrent.futures
 
 # python -m pip install windows-curses
 import curses
+import platform
 import random
 import shutil
+import sys
 import textwrap
 import time
+
+if platform.system() == "Windows":
+    from ctypes import windll
+
+    windll.kernel32.SetConsoleMode(windll.kernel32.GetStdHandle(-11), 7)
 
 
 class LLMAgent:
     def __init__(self, model="ChatGPT"):
         self.model = model
-        self.lines_printed = 0
+        # self.word_num = random.randint(20, 30)
+        self.word_num = 30
+        self.msg = f"[{self.model}]: I am {self.model}. ({self.word_num} words) "
+        # self.msg_to_lines()
+        self.count_msg_lines()
+
+    def msg_to_lines(self):
+        self.lines = textwrap.wrap(
+            self.msg,
+            width=shutil.get_terminal_size().columns,
+        )
+
+    def count_msg_lines(self):
+        terminal_width = shutil.get_terminal_size().columns
+        self.line_num = int(len(self.msg) / terminal_width) + 1
 
     async def answer(self, question=None):
-        word_num = random.randint(20, 30)
-        msg = f"[{self.model}]: I am {self.model}. ({word_num} words) "
-        print(msg)
-        for i in range(word_num):
-            rand_num = random.random() * 0.3
-            msg += f"<{self.model}_word_{i}>"
+        line_num = self.line_num
+        print(self.msg, end="", flush=True)
+        for i in range(self.word_num):
+            new_msg = f"<{self.model}_word_{i}>"
+            self.msg += new_msg
+            # self.msg_to_lines()
+            self.count_msg_lines()
+            rand_num = random.random() * 0.1
             await asyncio.sleep(rand_num)
-            lines = textwrap.wrap(
-                msg,
-                width=shutil.get_terminal_size().columns,
-            )
-            self.task_runner.max_lines = max(self.task_runner.max_lines, len(lines))
-            for j, line in enumerate(lines):
-                index = self.index + j + self.lines_printed
-                line = lines[j] if j < len(lines) else ""
-                try:
-                    curses.setupterm()
-                    curses.putp(curses.tparm(curses.tigetstr("cuu"), index + j))
-                    curses.putp(curses.tigetstr("el"))
-                    print(line, end="\r")
-                    curses.putp(curses.tparm(curses.tigetstr("cud"), index + j))
-                except Exception as e:
-                    print(f"Error: {e}")
-            self.lines_printed += len(lines) - 1
-        return msg
+            async with self.task_runner.lock:
+                self.task_runner.lock_counter += 1
+                print(new_msg, end="", flush=True)
+                if line_num == self.line_num:
+                    # print(self.lines[line_num - 1], end="\r")
+                    pass
+                else:
+                    # print(self.line_num)
+                    # print(self.lines[line_num], end="\r")
+                    # # for i in range(line_num):
+                    # #     print("\033[F")
+                    # # print("\033[K", end="\r")
+                    # for line in self.lines[line_num:]:
+                    #     print(f"{line}")
+                    line_num = self.line_num
+        print()
+
+        #     for line in self.lines:
+        #         print(f"\033[K{line}")
+        # for i in range(len(self.lines) + 1):
+        #     print("\033[F")
+        # print()
+        # print(self.lines, "\r")
+        # print(f"\033[K{self.msg}", end="\r")
 
 
 class TaskRunner:
     def __init__(self):
-        self.max_lines = 0
+        self.lock = asyncio.Lock()
+        self.agents = []
+        self.lock_counter = 0
+
+    def console_output(self):
+        msg = ""
+        for agent in self.agents:
+            msg += agent.msg
+        print(msg + "\r")
 
     async def ask_question(self, question="Hello, who are you?"):
         print(question)
         models = [
             "ChatGPT",
             "Sage",
-            "Claude-instant",
-            "Claude-instant-100k",
-            "Claude+",
-            "Bing",
+            # "Claude-instant",
+            # "Claude-instant-100k",
+            # "Claude+",
+            # "Bing",
         ]
         agents = [LLMAgent(model) for model in models]
         for i, agent in enumerate(agents):
-            agent.index = i * self.max_lines + 1
             agent.task_runner = self
+            self.agents.append(agent)
         tasks = [asyncio.create_task(agent.answer(question)) for agent in agents]
         await asyncio.gather(*tasks)
 
--- tests/test_curses.py
+++ tests/test_curses.py
@@ -2,7 +2,7 @@
 import curses
 
 
-def main(stdscr):
+def test_create_window_and_move(stdscr):
     # Turn off cursor blinking
     curses.curs_set(0)
 
@@ -25,6 +25,12 @@ def main(stdscr):
     # Print text with color attribute
     win.addstr(0, 0, "Hello, World!", curses.color_pair(1))
 
+    # Move the cursor and print more text
+    win.move(2, 0)
+    win.addstr("This is line 2.")
+    win.move(4, 0)
+    win.addstr("This is line 4.")
+
     # Refresh the screen
     win.refresh()
 
@@ -32,4 +38,9 @@ def main(stdscr):
     win.getch()
 
 
-curses.wrapper(main)
+def test_print_and_move(stdscr):
+    pass
+
+
+# curses.wrapper(test_create_window_and_move)
+curses.wrapper(test_print_and_move)
