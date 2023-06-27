d7e084428728fd1f472385753369ef7b0bc116b8
Hansimov <591172499@qq.com>
2023-06-27 19:09:46 +0800

    [Test] Fake data with Faker

--- tests/test_langchain_fake_llm.py
+++ tests/test_langchain_fake_llm.py
@@ -1,13 +1,47 @@
+#%%
+from faker import Faker
 from langchain.llms.fake import FakeListLLM
 from langchain.agents import load_tools, initialize_agent, AgentType
 
+
 tools = load_tools(["python_repl"])
-responses = [
-    "Action: Python REPL\nAction Input: print(2 + 2)",
-    "Final Answer: 4",
-    "Action: Wolfram REPL\nAction Input: sum(5, 10)",
-    "Final Answer: 15",
-]
+
+
+def generate_fake_emojis():
+    faker = Faker()
+    for i in range(10):
+        yield faker.emoji()
+
+
+fake_emojis = list(generate_fake_emojis())
+
+
+def generate_fake_jobs():
+    faker = Faker()
+    for i in range(10):
+        yield faker.job()
+
+
+fake_jobs = list(generate_fake_jobs())
+
+
+def generate_fake_responses():
+    responses = []
+    for i in range(10):
+        action_str = fake_jobs[i]
+        answer_str = fake_emojis[i]
+        responses.extend(
+            [
+                f"Action: Python REPL\nAction Input: print('{action_str}')",
+                f"Final Answer: {answer_str}",
+            ]
+        )
+    return responses
+
+
+responses = generate_fake_responses()
+print(responses)
+
 llm = FakeListLLM(responses=responses)
 agent = initialize_agent(
     tools,
@@ -15,5 +49,7 @@ agent = initialize_agent(
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
     verbose=True,
 )
-agent.run("whats 2 + 2")
-agent.run("Sum 5 and 10")
+
+
+for i in range(10):
+    agent.run("New Action")
