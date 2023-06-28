abedc27b6b307b60da4296911c7f434d84ee3189
Hansimov <591172499@qq.com>
2023-06-28 15:27:52 +0800

    [Feature] Animated paths with v-nework-graph

---
+++ webpage/src/MyComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <v-network-graph :nodes="nodes" :edges="edges" :layouts="layouts" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { Nodes, Edges, Layouts } from 'v-network-graph'
+import data from './data'
+
+export default defineComponent({
+  name: 'MyComponent',
+  data() {
+    return {
+      nodes: data.nodes as Nodes,
+      edges: data.edges as Edges,
+      layouts: data.layouts as Layouts
+    }
+  }
+})
+</script>
--- webpage/src/router/index.ts
+++ webpage/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
+import VisView from '@/views/VisView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,12 +16,12 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('/src/views/AboutView.vue')
+      component: () => import('@/views/AboutView.vue')
     },
     {
       path: '/vis',
       name: 'vis',
-      component: () => import('/src/views/VisView.vue')
+      component: VisView
     }
   ]
 })
