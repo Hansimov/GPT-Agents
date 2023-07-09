58afab0942f6747b5bccbc73c7df81c1e9098312
Hansimov <591172499@qq.com>
2023-07-10 02:18:14 +0800

    [Fix] Register vue-advanced-chat and emoji-picker

--- webpage/src/views/ChatView.vue
+++ webpage/src/views/ChatView.vue
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script>
 import { register } from 'vue-advanced-chat'
 register()
 
@@ -6,86 +6,13 @@ export default {
   data() {
     return {
       currentUserId: 'Asimov',
-      loadFirstRoom: true,
-      rooms: [
-        // {
-        //   _id: 'room1',
-        //   name: 'Room 1',
-        //   participants: ['user1', 'user2']
-        // },
-        // {
-        //   _id: 'room2',
-        //   name: 'Room 2',
-        //   participants: ['user1', 'user3']
-        // }
-      ],
+      rooms: [],
       currentRoomId: 'room1',
-      messages: [
-        // {
-        //   _id: 'message1',
-        //   content: 'Hello!',
-        //   senderId: 'user1',
-        //   roomId: 'room1'
-        // }
-        // {
-        //   _id: 'message2',
-        //   content: 'Hi!',
-        //   senderId: 'user2',
-        //   roomId: 'room1'
-        // }
-      ],
-      participants: [
-        // {
-        //   _id: 'user1',
-        //   username: 'User 1'
-        // },
-        // {
-        //   _id: 'user2',
-        //   username: 'User 2'
-        // },
-        // {
-        //   _id: 'user3',
-        //   username: 'User 3'
-        // }
-      ],
+      messages: [],
       roomActions: [
         { name: 'inviteUser', title: 'Invite User' },
         { name: 'removeUser', title: 'Remove User' },
         { name: 'deleteRoom', title: 'Delete Room' }
-      ],
-      menuActions: [
-        {
-          name: 'inviteUser',
-          title: 'Invite User'
-        },
-        {
-          name: 'removeUser',
-          title: 'Remove User'
-        },
-        {
-          name: 'deleteRoom',
-          title: 'Delete Room'
-        }
-      ],
-      messageActions: [
-        {
-          name: 'replyMessage',
-          title: 'Reply'
-        },
-        {
-          name: 'editMessage',
-          title: 'Edit Message',
-          onlyMe: true
-        },
-        {
-          name: 'deleteMessage',
-          title: 'Delete Message',
-          onlyMe: true
-        },
-        {
-          name: 'selectMessages',
-          title: 'Select'
-        }
       ]
     }
   }
@@ -94,10 +21,9 @@ export default {
 
 <template>
   <vue-advanced-chat
+    :current-user-id="currentUserId"
     :rooms="JSON.stringify(rooms)"
-    :current-room-id="currentRoomId"
     :messages="JSON.stringify(messages)"
-    :participants="participants"
     :room-actions="JSON.stringify(roomActions)"
   />
 </template>
--- webpage/vite.config.ts
+++ webpage/vite.config.ts
@@ -7,7 +7,15 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: tagName => {
+            return tagName === 'vue-advanced-chat' || tagName === 'emoji-picker'
+          }
+        }
+      }
+    }),
     vueJsx(),
   ],
   resolve: {
