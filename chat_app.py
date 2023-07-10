bae737af2fa70a0a2776adb871f82cf9041112cf
Hansimov <591172499@qq.com>
2023-07-10 20:03:48 +0800

    [Feature] Dump message via ChatApp

--- chat_app.py
+++ chat_app.py
@@ -1,7 +1,12 @@
+import hashlib
+import json
 import logging
-from termcolor import colored
+import time
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from pathlib import Path
+from termcolor import colored
+from datetime import datetime
 
 # logging.getLogger().setLevel(logging.INFO)
 
@@ -23,12 +28,14 @@ class ChatApp:
             print(colored(event_data, "yellow"))
             print(colored("Received.", "green"))
 
-            response = self.generate_message(event_data)
+            response = self.save_message(event_data)
 
             return jsonify(response)
 
-    def generate_message(self, event_data):
-        return {"message": "Received!"}
+    def save_message(self, event_data):
+        chat_dumper = ChatDumper()
+        chat_dumper.add_message(event_data)
+        return {"message": "Saved!"}
 
     def run(self):
         self.app.run(
@@ -36,6 +43,39 @@ class ChatApp:
             host=self.host,
             port=self.port,
         )
+
+
+class ChatDumper:
+    chat_json_path = (
+        Path(__file__).parent / "webpage" / "src" / "data" / "vue_chats.json"
+    )
+    message_id = 0
+
+    def __init__(self):
+        with open(self.chat_json_path, "r") as rf:
+            self.chat_data = json.load(rf)
+
+    def dump(self):
+        with open(self.chat_json_path, "w") as wf:
+            json.dump(self.chat_data, wf, indent=4)
+
+    def add_message(self, message):
+        message_datetime = datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        message_timestamp = str(int(datetime.now().timestamp()))
+        # message_id = hashlib.md5(message_timestamp.encode("utf-8")).hexdigest()
+        message_id = message_timestamp
+        sender_id = "0"
+        room_id = message["roomId"]
+        new_message = {
+            "_id": message_id,
+            "senderId": sender_id,
+            "content": message["content"],
+            # "date": message_datetime,
+            "timestamp": message_datetime,
+            "replyMessage": {},
+        }
+        self.chat_data["roomMessages"][room_id].append(new_message)
+        self.dump()
 
 
 if __name__ == "__main__":
