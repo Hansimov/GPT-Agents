fcb3e4e5ffaa550b4c2b19ebab4eb0f34da0e4e4
Hansimov <591172499@qq.com>
2023-07-14 19:15:26 +0800

    [Todo] GoogleSearcher

---
+++ plugins/google_searcher.py
@@ -0,0 +1,13 @@
+class GoogleSearcher:
+    """
+    Creating a Programmable Search Engine | Google for Developers
+      * https://developers.google.com/custom-search/docs/tutorial/creatingcse
+    Control Panel
+      * https://programmablesearchengine.google.com/controlpanel/all
+      * https://programmablesearchengine.google.com/controlpanel/overview?cx={...}
+    Custom Search JSON API
+        * https://developers.google.com/custom-search/v1/introduction
+    """
+
+    def __init__(self):
+        pass
