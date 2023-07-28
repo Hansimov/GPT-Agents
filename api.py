d288a0ca89532163cdbf1d247e2e7a0c6f4e5993
Hansimov <591172499@qq.com>
2023-07-28 15:33:34 +0800

    [Feature] Enable http api via AgentsApp

---
+++ api.py
@@ -0,0 +1,39 @@
+from flask import Flask, jsonify, request, Response
+from flask_cors import CORS
+from pymongo import MongoClient
+from agents.chimera_agent import ChimeraAgent
+from managers.chat_message_manager import ChatMessageManager
+
+
+class AgentsApp:
+    host = "127.0.0.1"
+    port = 8888
+
+    def __init__(self):
+        self.app = Flask(__name__)
+        CORS(self.app)
+        self.register_routes()
+
+    def register_routes(self):
+        @self.app.route("/api/chat", methods=["POST"])
+        def chat():
+            message = request.get_json()
+            print(message)
+            response_message = {
+                "status": "ok",
+                "content": "Hi I'm Chimera!",
+            }
+            print(response_message)
+            return jsonify(response_message)
+
+    def run(self):
+        self.app.run(host=self.host, port=self.port, debug=True)
+
+
+def main():
+    agents_app = AgentsApp()
+    agents_app.run()
+
+
+if __name__ == "__main__":
+    main()
