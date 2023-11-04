8af5618d71eaccb82f22d2f3b53a49b47f9690db
Hansimov <591172499@qq.com>
2023-11-04 12:26:18 +0800

    [Feature] load available models when page is loaded, and beautify ui

--- apps/js/submit_user_input.js
+++ apps/js/submit_user_input.js
@@ -1,32 +1,39 @@
 var source;
 
-$("#send-user-input-btn").click(function () {
-    var userInput = $("#userInput").val();
-    var url = "https://magic-api.ninomae.live/v1/models";
-
+$("#send-user-input").click(function () {
+    var userInput = $("#sent-user-input").val();
     send_user_input_btn = $(this);
-
     $(this).attr("disabled", true);
     $(this).removeClass("btn-success").addClass("btn-light");
-    available_models = $("#available-models");
+    $(this).text("Regenerate").removeClass("btn-light").addClass("btn-success").attr("disabled", false);
 
+});
+
+function load_available_models() {
+    var url = "https://magic-api.ninomae.live/v1/models";
+    available_models = $("#available-models");
+    current_model = $("#current-model");
     axios.get(url)
         .then(function (response) {
             console.log(response.data);
             var row = $('<div>').addClass('grid');
-            response.data.data.forEach(function (item) {
+            response.data.data.sort(function (a, b) {
+                return a.id.localeCompare(b.id);
+            }).forEach(function (item) {
                 var col = $('<span>').addClass('available-model-item border p-1 m-0').append($('<span>').text(item.id));
+                col.click(function () {
+                    current_model.text(`Chat with: [${item.id}]`);
+                    source = item.id;
+                });
                 row.append(col);
             });
-
             available_models.html(row);
-
-            send_user_input_btn.text("Regenerate");
-            send_user_input_btn.removeClass("btn-light").addClass("btn-success");
-            send_user_input_btn.attr("disabled", false);
         })
         .catch(function (error) {
             console.log(error);
         });
+}
 
+$(document).ready(function () {
+    load_available_models();
 });
