583fb67fdcbf22470b008147bf34aaf185d4acdb
Hansimov <591172499@qq.com>
2023-11-06 16:07:18 +0800

    :beers: [Feature] Bind functions with enter to user input

--- apps/llm_mixer/js/buttons_binder.js
+++ apps/llm_mixer/js/buttons_binder.js
@@ -7,33 +7,48 @@ export class ButtonsBinder {
     bind_send_user_input() {
         const button = $("#send-user-input");
         button.click(async () => {
+            await this.handle_user_input(button);
+        });
+
+        $("#user-input").keypress(async (event) => {
             let status = button.text().trim();
-            if (status === "Send" || status === "Regenerate") {
-                console.log("Send");
-                button
-                    .text("Stop")
-                    .removeClass("btn-primary btn-success")
-                    .addClass("btn-warning");
-                await this.post_user_input();
-                button
-                    .text("Regenerate")
-                    .removeClass("btn-warning")
-                    .addClass("btn-success")
-                    .attr("disabled", false);
-            } else if (status === "Stop") {
-                console.log("Stop");
-                this.requester.stop();
-                button
-                    .attr("disabled", true)
-                    .removeClass("btn-warning")
-                    .addClass("btn-success")
-                    .text("Regenerate")
-                    .attr("disabled", false);
-                return;
-            } else {
-                console.log("No action");
+            if (
+                event.key === "Enter" &&
+                !event.shiftKey &&
+                (status === "Send" || status === "Regenerate")
+            ) {
+                event.preventDefault();
+                await this.handle_user_input(button);
             }
         });
+    }
+    async handle_user_input(button) {
+        let status = button.text().trim();
+        if (status === "Send" || status === "Regenerate") {
+            console.log("Send");
+            button
+                .text("Stop")
+                .removeClass("btn-primary btn-success")
+                .addClass("btn-warning");
+            await this.post_user_input();
+            button
+                .text("Regenerate")
+                .removeClass("btn-warning")
+                .addClass("btn-success")
+                .attr("disabled", false);
+        } else if (status === "Stop") {
+            console.log("Stop");
+            this.requester.stop();
+            button
+                .attr("disabled", true)
+                .removeClass("btn-warning")
+                .addClass("btn-success")
+                .text("Regenerate")
+                .attr("disabled", false);
+            return;
+        } else {
+            console.log("No action");
+        }
     }
     async post_user_input() {
         let user_input = $("#user-input").val();
